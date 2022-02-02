bf95d040e6174691dee74b4d18b5c2ba4ff64cd2
Kaustubh Trivedi <kaus12tri@gmail.com>
2022-02-02 12:49:48 +0530

    Tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  plugins: ["postcss-import", "tailwindcss", "autoprefixer"],
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        "2/3": "66.666667%",
+      },
+    },
+  },
 };
