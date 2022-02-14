4b8070e2f60f2e40d55c843aa4e413cc5c3020ab
Kaustubh Trivedi <kaus12tri@gmail.com>
2022-02-14 16:52:22 +0530

    downgraded tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
   ],
-  mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
